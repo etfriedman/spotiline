acf5e1be75e9752e5adc89d6bd9524ca44bf562a
Ethan Friedman <etfriedman@packer.edu>
2020-06-25 16:33:37 -0400

    removed old folder

---
+++ main.py
@@ -0,0 +1,4 @@
+#imports
+from bs4 import BeautifulSoup # awesome library for webscraping, less powerful than scrapy, easier to use.
+import requests # allows us to make requests to websites
+import re # regex (regular expressions)
